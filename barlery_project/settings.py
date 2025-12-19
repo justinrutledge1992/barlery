a759c3883c9f7208b05d96fd4b82145c0ee7b435
Justin Rutledge <justin@jrutledge.net>
2025-12-19 12:15:13 -0500

    updated DEBUG env var settings

--- barlery_project/settings.py
+++ barlery_project/settings.py
@@ -19,7 +19,7 @@ DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", True)
 
 ALLOWED_HOSTS = []
 
